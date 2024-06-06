2ba95f06f1bb9c151605022ad967f7839da1620c
vishnu192003 <chacherkarvishnu@gmail.com>
2024-06-06 13:37:09 +0530

    meet2

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import Img1 from '../IMG_20240511_221507_668.jpg'
 import Img2 from '../46.jpg'
 import Img3 from '../47.jpg'
 import Img4 from '../48.jpg'
+import Mic from '../mute.jpg';
+import Mic1 from '../mic.jpg'   
 
 const Main = () => {
     return (
@@ -11,21 +13,25 @@ const Main = () => {
             <div className="video-grid">
                 {/* Example video tiles */}
                 <div className="video-tile">
+                <img className='mic' src={Mic} alt="" />
                 <img className='student-photo' src={Img1} alt="" />
                 <h3 className='student-name'>VISHNU</h3>
                      </div>
-                
-                     <div className="video-tile">
+
+                <div className="video-tile">
+                <img className='mic' src={Mic1} alt="" />
                 <img className='student-photo' src={Img2} alt="" />
                 <h3 className='student-name'>LEENA</h3>
 
                      </div>
                      <div className="video-tile">
+                     <img className='mic' src={Mic} alt="" />     
                 <img className='student-photo' src={Img3} alt="" />
                 <h3 className='student-name'>JON</h3>
 
                      </div>
                      <div className="video-tile">
+                     <img className='mic' src={Mic} alt="" />
                 <img className='student-photo' src={Img4} alt="" />
                 <h3 className='student-name'>EMMA</h3>
 
